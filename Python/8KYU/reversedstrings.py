c826e80cfc80184f45c7adc3cbd05741edf318e6
Leahnicoles <lnsimonn@hotmail.com>
2023-04-16 23:17:24 +0100

    add reversed strings kata

---
+++ Python/8KYU/reversedstrings.py
@@ -0,0 +1,12 @@
+"""
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+"""
+
+def solution(string):
+    return string[::-1] 
+
+# strings are immutable, thats why cant use reverse method.
