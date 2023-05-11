5ffc3c623606700d3bc14eced01e36e81e4de32e
Leahnicoles <lnsimonn@hotmail.com>
2023-05-11 11:56:53 +0100

    add ruby boolean to string kata

---
+++ Ruby/8KYU/booleantostring.rb
@@ -0,0 +1,13 @@
+=begin
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+=end
+
+def boolean_to_string(b)
+  return b.to_s
+end
+
+def boolean_to_string(b)
+  return "#{b}"
+end
