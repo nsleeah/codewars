6b91398106ac6047f1630985616b01c819d25314
Leahnicoles <lnsimonn@hotmail.com>
2023-05-20 01:44:11 +0100

    add ruby function syntax debug kata

---
+++ Ruby/8KYU/functionsyntaxdebugging.rb
@@ -0,0 +1,14 @@
+=begin
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+=end
+
+def main[verb, noun]
+    return ;
+    verb + noun
+}
+
+#Answer
+def main(verb, noun)
+    return verb + noun
+end
