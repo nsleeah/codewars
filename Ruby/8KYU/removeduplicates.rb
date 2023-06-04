0775f2e1129b4cd366c4db5677f51e63f9e6d0ac
Leahnicoles <lnsimonn@hotmail.com>
2023-06-04 22:45:47 +0100

    add ruby remove duplicates from list kata

---
+++ Ruby/8KYU/removeduplicates.rb
@@ -0,0 +1,12 @@
+=begin
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+=end
+
+
+def distinct(seq)
+    return seq.uniq
+end
+
+#Uniq Returns Array by Removing Duplicate Values in the Input.
