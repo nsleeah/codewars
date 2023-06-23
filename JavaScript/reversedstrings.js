e88304508f5f220984ce29e8d80b4c6899172462
Leahnicoles <lnsimonn@hotmail.com>
2023-06-23 20:47:57 +0100

    add js reversed string kata

---
+++ JavaScript/reversedstrings.js
@@ -0,0 +1,15 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+function solution(str){
+    return str.split("").reverse().join("");
+}
+  
+  //No Built In Method To Reverse String, But Can Reverse Array?
+  //Convert To Array.
+  //Reverse.
+  //Return Back To String.
